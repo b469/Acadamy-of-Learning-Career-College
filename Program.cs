8833510ab6c8cd3e6f06a190f3ea4639d5adc666
luis bolivar <bolivarluis97@yahoo.ca>
2026-03-08 16:59:10 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acadamy_of_Learning_Career_College
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Academy of Learning Career College!"); 
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("Waht is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string pageNumber = Console.ReadLine();
+            int pageNum = Convert.ToInt32(pageNumber);
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            string help = Console.ReadLine();
+            bool helpBool = Convert.ToBoolean(help);
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string experience = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+            int hoursNum = Convert.ToInt32(hours);
+                Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            
+
+
+
+
+
+
+
+        }
+    }
+}
